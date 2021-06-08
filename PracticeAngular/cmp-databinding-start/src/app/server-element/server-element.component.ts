bd53694a3c6b81f9213667edb54c37ef1b302f7a
matiweber <matias.weber@globant.com>
2021-06-08 13:09:55 -0300

    add hooks in databinding

--- PracticeAngular/cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ PracticeAngular/cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,19 +1,29 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChange, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
-  // encapsulation: ViewEncapsulation.
+  // encapsulation: ViewEncapsulation.Emulated
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges {
 
- @Input('svElement') element: {type: string, name: string, content: string};
+  @Input('svElement') element: {type: string, name: string, content: string};
+  @Input() name: string;
 
-  constructor() { }
+  constructor() { 
+    console.log( 'constructor called' )
+  }
+
+  ngOnChanges(changes: SimpleChange) {
+    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    //Add '${implements OnChanges}' to the class.
+    console.log('ngOnChanges!!!');
+    console.log(changes);
+  }
 
   ngOnInit(): void {
-
+    console.log('ng onInit called!!!');
   }
 
 }
