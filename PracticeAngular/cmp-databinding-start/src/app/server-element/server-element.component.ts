898b4e4cbaae89a964febebb02e46915810259c9
matiweber <matias.weber@globant.com>
2021-06-08 16:49:56 -0300

    recipeApp modified, not working

--- PracticeAngular/cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ PracticeAngular/cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,8 @@
-import { Component, Input, OnChanges, OnInit, SimpleChange, ViewEncapsulation } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit,
+   Component, ContentChild, DoCheck, ElementRef, Input, OnChanges, OnDestroy, 
+   OnInit, SimpleChanges, ViewChild, ViewEncapsulation
+
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,24 +10,59 @@ import { Component, Input, OnChanges, OnInit, SimpleChange, ViewEncapsulation } 
   styleUrls: ['./server-element.component.css']
   // encapsulation: ViewEncapsulation.Emulated
 })
-export class ServerElementComponent implements OnInit, OnChanges {
+export class ServerElementComponent implements
+ OnInit, OnChanges, DoCheck, AfterContentInit,
+ AfterViewChecked, AfterContentChecked, 
+ AfterViewInit, OnDestroy
+
+ {
 
   @Input('svElement') element: {type: string, name: string, content: string};
   @Input() name: string;
+  @ViewChild('heading') header: ElementRef;
+  @ContentChild('contentParagraph') paragraph: ElementRef;
 
   constructor() { 
     console.log( 'constructor called' )
   }
 
-  ngOnChanges(changes: SimpleChange) {
+  ngOnChanges(changes: SimpleChanges) {
     //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
     //Add '${implements OnChanges}' to the class.
     console.log('ngOnChanges!!!');
     console.log(changes);
   }
 
-  ngOnInit(): void {
-    console.log('ng onInit called!!!');
+  ngOnInit() {
+    console.log('ngOnInit called!!!');
+    console.log(this.header.nativeElement.textContent);
+    console.log('text content of paragraph' + this.paragraph.nativeElement.textContent);
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck is Heree!');
+  }
+
+  ngAfterContentInit() {
+    console.log('afterContentINIT CALLED');
+  }
+
+  ngAfterContentChecked() {
+    //Called after every check of the component's or directive's content.
+    //Add 'implements AfterContentChecked' to the class.
+    
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit ');
+  }
+
+  ngAfterViewChecked() {
+    console.log('after view check NG');
+  }
+
+  ngOnDestroy() {
+    console.log('OnDestroy!!!!')
   }
 
 }
--- PracticeAngular/recipeApp/src/app/app.component.ts
+++ PracticeAngular/recipeApp/src/app/app.component.ts
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   
 })
 export class AppComponent {
-  
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
