6caece68ab9dc68c60e66cb3b1f8b132e70c6bcf
matiweber <matias.weber@globant.com>
2021-06-08 11:01:20 -0300

    fix folder

---
+++ PracticeAngular/recipeApp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+ 
+  recipes: Recipe[] = [
+    new Recipe('Recipe desert test', 'Description test Recipe', 'https://www.aspicyperspective.com/wp-content/uploads/2020/05/Easy-Waffle-Recipe-11-650x957.jpg')
+  ];
+
+  constructor() {
+
+   }
+
+  ngOnInit(): void {
+
+  }
+
+}
